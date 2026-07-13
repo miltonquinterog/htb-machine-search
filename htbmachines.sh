79903702e57b29e4809ca5a2c75a5e50c9da29d5
Milton Quintero <quinterogiraldomilton@gmail.com>
2026-07-13 15:24:09 -0400

    Add YouTube walkthrough lookup for HTB machines

--- htbmachines.sh
+++ htbmachines.sh
@@ -26,6 +26,7 @@ function helpPanel(){
   echo -e "\t${yellowColour}u)${endColour}${grayColour} Descargar o actualizar archivos necesarios${endColour}"
   echo -e "\t${yellowColour}m)${endColour}${grayColour} Buscar por un nombre de maquina${endColour}"
   echo -e "\t${yellowColour}i)${endColour}${grayColour} Buscar por direccion IP${endColour}"
+  echo -e "\t${yellowColour}y)${endColour}${grayColour} Obtener el link de la resolucion de la maquina en Youtube${endColour}"
   echo -e "\t${yellowColour}h)${endColour}${grayColour} Mostrar este panel de ayuda${endColour}\n"
 }
 
@@ -34,14 +35,14 @@ function updateFiles(){
   if [ ! -f bundle.js ]; then
     tput civis
     echo -e "\n${yellowColour}[+]${endColour}${grayColou} Descargando archivos necesarios...${endColour}"
-    curl -s "$main_url > bundle.js"
+    curl -s "$main_url" > bundle.js
     js-beautify bundle.js | sponge bundle.js
     echo -e "\n${yellowColour}[+]${endColour}${grayColour} Todos los archivos han sido descargados${endColour}"
     tput cnorm
   else
     tput civis
     echo -e "\n${yellowColour}[+]${endColour}${grayColour} Comprobando si hay actualizaciones pendientes...${endColour}"
-    curl -s "$main_url > bundle.temp.js"
+    curl -s "$main_url" > bundle_temp.js
     js-beautify bundle_temp.js | sponge bundle_temp.js
     md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
     md5_original_value=$(md5sum bundle.js | awk '{print $1}')
@@ -65,15 +66,65 @@ function updateFiles(){
 function searchMachine(){
   machineName="$1"
 
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando las propiedades de la maquina${endColour}\n"
+  machineName_checker=$(cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//')
 
-cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d  '"' | tr -d ',' | sed 's/^ *//'
+  if [ "$machineName_checker" ]; then
+
+
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} Listando las propiedades de la maquina${endColour}\n"
+
+    cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d  '"' | tr -d ',' | sed 's/^ *//'
+  else
+     echo -e "\n${redColour}[!] La maquina proporcionada no existe${endColour}\n"
+  fi
 }
 
 function searchIP(){
   ipAddress="$1"
 
-  machineName="$(cat bundle.js | grep "ip: \"$ipAddress\"" -B 3 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')"
+  machineName=$(cat bundle.js | grep "ip: \"$ipAddress\"" -B 3 | grep "name: " | awk 'NF{print $NF}' | tr -d '"' | tr -d ',')
 
-  echo -e "\n${yellowColour}[+]${endColour}${grayColour} La Maquina correspondiente para la IP${endColour}${blueColour} $ipAddress${endColour}${grayColour} es${endColour}${purpleColour} $machineName${endColour}\n"
+  if [ "$machineName" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} La Maquina correspondiente para la IP${endColour}${blueColour} $ipAddress${endColour}${grayColour} es${endColour}${purpleColour} $machineName${endColour}\n"
+  else 
+    echo -e "\n${redColour}[!] La direccion IP proporcionada no existe${endColour}"
+  fi
 }
+
+function getYoutubeLink(){
+  machineName="$1"
+
+  youtubeLink=$(cat bundle.js | awk "/name: \"$machineName\"/,/resuelta:/" | grep -vE "id:|sku:|resuelta" | tr -d '"' | tr -d ',' | sed 's/^ *//' | grep youtube | awk 'NF{print $NF}')
+
+  if [ "$youtubeLink" ]; then
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour} El tutorial para la maquina esta en el siguiente enlace:${endColour}${blueColour} $youtubeLink${endColour}"
+  else
+    echo -e "\n${redColour}[!] La maquina proporcionada no existe${endColour}"
+  fi
+}
+
+# Indicadores
+
+declare -i parameter_counter=0
+
+while getopts "m:ui:y:h" arg; do
+  case $arg in
+    m) machineName=$OPTARG; let parameter_counter+=1;;
+    u) let parameter_counter+=2;;
+    i) ipAddress=$OPTARG; let parameter_counter+=3;;
+    y) machineName=$OPTARG; let parameter_counter+=4;;
+    h) ;;
+  esac
+done
+
+if [ "$parameter_counter" -eq 1 ]; then
+  searchMachine "$machineName"
+elif [ "$parameter_counter" -eq 2 ]; then
+  updateFiles
+elif [ "$parameter_counter" -eq 3 ]; then
+  searchIP "$ipAddress"
+elif [ "$parameter_counter" -eq 4 ]; then
+  getYoutubeLink "$machineName"
+else
+  helpPanel
+fi
